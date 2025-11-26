d0e6c5840749d27f09793771927a3180cea8def2
Cookie <2389397239@qq.com>
2025-11-26 20:23:47 +0700

    ScannerSettings now carries TrackMetadataParser::Parameters plus a configurable album-artist fallback flag

--- src/libs/services/scanner/impl/ScannerSettings.hpp
+++ src/libs/services/scanner/impl/ScannerSettings.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "scanners/audiofile/TrackMetadataParser.hpp"
+
 namespace lms::scanner
 {
     /**
@@ -7,8 +9,10 @@ namespace lms::scanner
      */
     struct ScannerSettings
     {
-        bool enableOptimize{ true }; // 是否执行优化步骤
-        bool enableCompact{};        // 是否执行压缩步骤
+        bool enableOptimize{ true };                // 是否执行优化步骤
+        bool enableCompact{};                       // 是否执行压缩步骤
+        bool preferAlbumArtistFallback{ true };     // 无 artist 时是否回退到 album artist
+        TrackMetadataParser::Parameters metadataParserParameters; // 元数据解析参数
     };
 } // namespace lms::scanner
 
