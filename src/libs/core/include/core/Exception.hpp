a62efcb5f91d878db748e7dcba9fdd88ea4e153c
Cookie <2389397239@qq.com>
2025-12-02 19:38:46 +0700

    编译成功

---
+++ src/libs/core/include/core/Exception.hpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2018 Emeric Poupon
+ *
+ * This file is part of LMS.
+ *
+ * LMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LMS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+namespace lms::core
+{
+    // 通用异常类型，供各模块（如 som、subsonic 等）复用
+    class LmsException : public std::runtime_error
+    {
+    public:
+        explicit LmsException(std::string_view error = "")
+            : std::runtime_error{ std::string{ error } } {}
+    };
+} // namespace lms::core
+
+
